224f296137d521a5cd9ce96320569b4433f84b02
lycho33 <cho.lyds@gmail.com>
2021-09-01 11:33:43 -0400

    commit tree 7d76c02201280432082d2b80d71a627e459c7d1c

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Code your solution in this file!
+// const returnFirstTwoDrivers = function(drivers) {
+//     return drivers.slice(0, 2)
+// }
+
+// const returnLastTwoDrivers = function(drivers) {
+//     return drivers.slice(-2)
+// }
+
+// const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+// function createFareMultiplier(ride) {
+//     return function multiplyFare(fare) {
+//         return ride * fare
+//     }
+// }
+
+// const fareDoubler = createFareMultiplier(2) //2 = fare ---- fareDoubler(10) -> 10 = ride --> ride * fare = 10 * 2
+// const fareTripler = createFareMultiplier(3)
+
+// const selectDifferentDrivers = function(arrayOfDrivers, selectFunctions) {
+//   return selectFunctions(arrayOfDrivers)
+//   //selectFunctions = returnFirstTwoDriver + retrunLastTwoDrivers
+// }
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+function createFareMultiplier(integer){
+    return function(fare){
+        return integer * fare
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(arrayOfDrivers, selectFunctions) {
+  return selectFunctions(arrayOfDrivers)
+  selectFunctions = returnFirstTwoDrivers + retrunLastTwoDrivers
+}
